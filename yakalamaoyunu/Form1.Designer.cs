ced7dde445e2f275465924f1847e1c4e0610821e
İbrahim Emrullah Pala <155005322+1PALA1@users.noreply.github.com>
2024-09-24 01:20:01 +0300

    Add files via upload

---
+++ yakalamaoyunu/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace yakalamaoyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.elmapic = new System.Windows.Forms.PictureBox();
+            this.sepetpic = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelhak = new System.Windows.Forms.Label();
+            this.tekrarbuton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.elmapic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sepetpic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // elmapic
+            // 
+            this.elmapic.BackColor = System.Drawing.Color.Red;
+            this.elmapic.Image = ((System.Drawing.Image)(resources.GetObject("elmapic.Image")));
+            this.elmapic.Location = new System.Drawing.Point(100, 0);
+            this.elmapic.Name = "elmapic";
+            this.elmapic.Size = new System.Drawing.Size(30, 30);
+            this.elmapic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.elmapic.TabIndex = 0;
+            this.elmapic.TabStop = false;
+            // 
+            // sepetpic
+            // 
+            this.sepetpic.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.sepetpic.Image = ((System.Drawing.Image)(resources.GetObject("sepetpic.Image")));
+            this.sepetpic.Location = new System.Drawing.Point(234, 390);
+            this.sepetpic.Name = "sepetpic";
+            this.sepetpic.Size = new System.Drawing.Size(120, 30);
+            this.sepetpic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sepetpic.TabIndex = 1;
+            this.sepetpic.TabStop = false;
+            this.sepetpic.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelhak
+            // 
+            this.labelhak.AutoSize = true;
+            this.labelhak.Location = new System.Drawing.Point(12, 424);
+            this.labelhak.Name = "labelhak";
+            this.labelhak.Size = new System.Drawing.Size(121, 16);
+            this.labelhak.TabIndex = 2;
+            this.labelhak.Text = "TOPLAM ELMA = 0";
+            this.labelhak.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.labelhak.Click += new System.EventHandler(this.labelhak_Click);
+            // 
+            // tekrarbuton
+            // 
+            this.tekrarbuton.BackColor = System.Drawing.Color.DarkGray;
+            this.tekrarbuton.Location = new System.Drawing.Point(461, 424);
+            this.tekrarbuton.Name = "tekrarbuton";
+            this.tekrarbuton.Size = new System.Drawing.Size(119, 26);
+            this.tekrarbuton.TabIndex = 3;
+            this.tekrarbuton.Text = "Tekrar Oyna";
+            this.tekrarbuton.UseVisualStyleBackColor = false;
+            this.tekrarbuton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(581, 450);
+            this.Controls.Add(this.tekrarbuton);
+            this.Controls.Add(this.labelhak);
+            this.Controls.Add(this.sepetpic);
+            this.Controls.Add(this.elmapic);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.elmapic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sepetpic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox elmapic;
+        private System.Windows.Forms.PictureBox sepetpic;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelhak;
+        private System.Windows.Forms.Button tekrarbuton;
+    }
+}
+
---
+++ yakalamaoyunu/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yakalamaoyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int X_sepet = 0, Y_sepet = 0;
+        int X_elma = 0, Y_elma = 0;
+        int toplama = 0;
+        int elmahizi=5;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tekrarbuton.Visible= false;
+            Y_elma = Y_elma + elmahizi;
+            
+
+            elmapic.Location = new Point(X_elma,Y_elma);
+
+            Random random = new Random();
+
+            if (X_sepet < X_elma + 30 && X_sepet + 120 > X_elma && Y_sepet < Y_elma + 30)
+            {
+                X_elma = random.Next(0, 400);
+                Y_elma = 0;
+                toplama++;
+                labelhak.Text = "TOPLAM ELMA = " + toplama;
+                if (timer1.Interval > 10)
+                {
+                    timer1.Interval = timer1.Interval - 3;
+                }
+            }
+            else if (Y_elma + 10 > 320) 
+            {
+                Y_elma = 0;
+                timer1.Enabled= false;
+                tekrarbuton.Visible = true;
+
+            } 
+
+                
+
+        }
+
+        private void labelhak_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            toplama = 0;
+            labelhak.Text = "TOPLAM ELMA = " + toplama;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int X = e.X;
+            int Y = e.Y;
+
+            X_sepet = X;
+            Y_sepet = 300;
+
+            sepetpic.Location=new Point(X_sepet, Y_sepet);
+
+        }
+    }
+}
